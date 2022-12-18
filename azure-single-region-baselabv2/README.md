b050171b455c68f51fa3304f41c71b72bc2b95fb
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2022-12-18 07:13:16 +0000

    Provider Updates & Baselab V2 Module

---
+++ azure-single-region-baselabv2/README.md
@@ -0,0 +1,3 @@
+# Single Region Base Lab Environment for Azure - Version 2 (Module)
+
+For an overview of this environment, please see (here)[]
---
+++ azure-single-region-baselabv2/azuredeploy.tf
@@ -0,0 +1,44 @@
+#Providers
+terraform {
+  required_providers {
+    azurerm = {
+      # Specify what version of the provider we are going to utilise
+      source  = "hashicorp/azurerm"
+      version = ">= 3.36.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4.3"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+  }
+}
+# Modules 
+module "single-region-baselabv2" {
+  source = "./modules/single-region-baselabv2"
+  # Tags
+  environment_tag = "jakewalsh90-baselab-v2"
+  # Regional
+  region1     = "uksouth"
+  region1code = "uks"
+  # Networking 
+  # Note - changing to anything other than /19 will require checking IP ranges/addressing within the Code. 
+  region1cidr = "10.10.0.0/19"
+  # Identity VMs
+  # Note - 1 VM is the minimum
+  vmcount   = "1"
+  vmsize    = "Standard_D2s_v4"
+  adminuser = "labadmin"
+  # Optional Features
+  # Azure Bastion
+  bastion = true
+  # AVD Supporting Elements
+  avd = true
+  # Virtual Network Gateway
+  vng = true
+  # Firewall
+  azfw = true
+}
