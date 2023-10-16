9f52130cccd3eafb791d7c4cae3ab660e73ac0a0
Jake Walsh <37653943+jakewalsh90@users.noreply.github.com>
2023-10-16 08:37:43 +0100

    Update AzureRM Provider to 3.76.0
    Update AzureRM Provider to 3.76.0

--- azure-single-region-baselabv2/azuredeploy.tf
+++ azure-single-region-baselabv2/azuredeploy.tf
@@ -4,7 +4,7 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source  = "hashicorp/azurerm"
-      version = ">= 3.75.0"
+      version = ">= 3.76.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -42,6 +42,8 @@ module "single-region-baselabv2" {
   # Firewall
   azfw = true
 }
+
+
 
 
 
